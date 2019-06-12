f50fe8ef41a345e97a915831fb4291ab733afb8a
uracha <uracha_p@kkumail.com>
2019-06-12 13:41:32 +0700

    Edit : แก้ไข Expected Result บน Test Scenario ของโม

--- ATDD/main.go
+++ ATDD/main.go
@@ -9,11 +9,11 @@ import (
 func main (){
 	http.HandleFunc("/duration", func (w http.ResponseWriter, r *http.Request)  {
 		w.Header().Set("Content-Type","application/json")
-		fmt.Fprintf(w, `{"days":"7,907 days",
+		fmt.Fprintf(w, `{"days":"7907 days",
 		"startFullDate":"Thursday, 16 October 1997",
 		"endFullDate":"Monday, 10 June 2019",
 		"dayOfMonths":"259 months, 25 days",
-		"dayOfWeeks":"1,129 weeks, 4 days",
+		"dayOfWeeks":"1129 weeks, 4 days",
 		"seconds":"683,164,800 seconds",
 		"minutes":"11,386,080 minutes",
 		"hours":"189,768 hours"}`)
